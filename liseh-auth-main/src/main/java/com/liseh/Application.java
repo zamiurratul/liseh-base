6a779aa100fe81ab9ea937cd78f16c7e3b5e227f
Zamiur Rahman Ratul <zamiurratul@gmail.com>
2019-10-09 18:02:56 +0600

    Remove unnecessary Beans

--- liseh-auth-main/src/main/java/com/liseh/Application.java
+++ liseh-auth-main/src/main/java/com/liseh/Application.java
@@ -8,25 +8,4 @@ public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public LocaleResolver localeResolver() {
-        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
-        return localeResolver;
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean localValidatorFactoryBean() {
-        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-        bean.setValidationMessageSource(messageSource());
-        return bean;
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:locale/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
 }
